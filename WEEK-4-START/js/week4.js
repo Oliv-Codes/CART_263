5a0892ea0efed4bb3bf3d0f9676311a10ab46b83
Oliv-Codes <230441967+Oliv-Codes@users.noreply.github.com>
2026-02-03 13:22:41 -0800

    lecture

---
+++ WEEK-4-START/js/week4.js
@@ -0,0 +1,26 @@
+window.onload = setup;
+function setup() {
+  console.log("in week 4 ;)")
+  
+  document.querySelector("#boxA").addEventListener("click", runTimeout)
+
+     window.setInterval(addTextRecur,2000);
+    function addTextRecur(){ 
+    let parent = document.getElementById("parent");
+        parent.innerHTML+=" NEW TEXT TO APPEAR RECUR ";
+  }
+    
+    function runTimeout (){
+      window.setTimeout(addTimeoutText,2000);
+    }
+    
+
+  function addTimeoutText(){
+    let parent = document.getElementById("parent");
+    parent.innerHTML+=" NEW TEXT TO APPEAR ";
+  }
+
+
+  
+}
+
