39c63b0bbfd11bf4004a6588a06138fa4b858e19
Oliv-Codes <230441967+Oliv-Codes@users.noreply.github.com>
2026-02-17 14:04:13 -0800

    lectures complete

---
+++ CART263-OBJECTS-III/js/Bird.js
@@ -0,0 +1,83 @@
+class Bird extends Animal{
+  // Create a new bird object that moves to the right
+  constructor(x, y,width,height) {
+    super(x,y,width,height)
+    // this.x = x;
+    // this.y = y;
+    // this.width = width;
+    // this.height = height;
+    this.vx = Math.random() * 5 + 1;
+    this.vy = 0;
+    this.animalBody = document.createElement("div");
+
+    //ONLY in the Bird class : new variables
+    this.angle = 0;
+    this.sleepiness = 0.1;
+    this.originalY = this.y;
+  }
+
+  move() {
+    //console.log("go");
+    
+    this.y=this.originalY + Math.sin(this.angle)*30
+    this.angle+=0.05;
+    this.veer();
+    super.move();
+  }
+
+  veer() {
+    let r = Math.random();
+    //console.log("in veer "+r)
+    if (r < this.sleepiness) {
+      this.vy += randomRange(-.1, .1);
+    }
+}
+
+wrap() {
+    // NEW! Call the superclass (Animal) version of wrap()
+    // This will handle wrapping on the x axis for us.
+    // We use "super" to access methods in the SUPERclass (Animal)
+    // So this calls the Animal version of wrap() after we make the specific changes 
+    // in the SUBclass.
+    //this resets velocity
+   
+    if (this.x > window.innerWidth) {
+      //reset
+      this.vy = 0;
+    }
+     super.wrap();
+   }
+  
+  // Move the bird according to its velocity
+//   move() {
+//     this.x += this.vx;
+//     this.y += this.vy;
+//     //update the actual div...
+//     this.animalBody.style.left = this.x + "px";
+//     this.animalBody.style.top = this.y + "px";
+//   }
+
+  // Wrap the bird if it reaches the right edge (checks window, brings brid back to zero)
+//   wrap() {
+//     if (this.x > window.innerWidth) {
+//       this.x -= window.innerWidth;
+//     }
+//   }
+
+ // Display the bird as a ellipse
+  renderAnimal() {
+    super.renderAnimal();
+    // this.animalBody.classList.add("animal");
+    // this.animalBody.style.width = this.width + "px";
+    // this.animalBody.style.height = this.height + "px";
+    // this.animalBody.style.left = this.x + "px";
+    // this.animalBody.style.top = this.y + "px";
+    this.animalBody.style.borderRadius = this.width + "px";
+    this.animalBody.style.backgroundColor = `rgb(106, 90, 205)`;
+    //add to the DOM
+    document.getElementsByClassName("sky")[0].appendChild(this.animalBody); 
+  }
+}
+function randomRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
