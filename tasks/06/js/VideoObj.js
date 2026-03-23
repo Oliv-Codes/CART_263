fcf9158a3e93bff570acc6d17f964d074b05c845
Oliv-Codes <230441967+Oliv-Codes@users.noreply.github.com>
2026-03-22 23:32:23 -0400

    added sepia and colour

--- tasks/06/js/VideoObj.js
+++ tasks/06/js/VideoObj.js
@@ -10,7 +10,7 @@ class VideoObj {
     this.shapeY =10;
     this.shapeCol = "#000000";
  
-
+    //BLUR
     let filterButton_blur = document.getElementById("filter_button_blur");
     let blurInput = document.getElementById("blurnum");
     this.userProvidedBlur  = 0;
@@ -23,11 +23,32 @@ class VideoObj {
       console.log(self.userProvidedBlur);
     });
 
-    let filterButton_invert = document.getElementById("filter_button_invert");
-let invertInput = document.getElementById("invertnum");
-this.userProvidedInvert = 0;
+    //SEPIA
+    let filterButton_sepia = document.getElementById("filter_button_sepia");
+    let sepiaInput = document.getElementById("sepianum");
+    this.userProvidedSepia = 0;
 
-filterButton_invert.addEventListener("click", function () {
+    filterButton_sepia.addEventListener("click", function () {
+    self.userProvidedSepia = sepiaInput.value;
+    console.log("sepia:", self.userProvidedSepia);
+    });
+
+    //COLOUR
+    let filterButton_hue = document.getElementById("filter_button_hue");
+    let hueInput = document.getElementById("huenum");
+    this.userProvidedHue = 0;
+
+    filterButton_hue.addEventListener("click", function () {
+    self.userProvidedHue = hueInput.value;
+    console.log("hue-rotate:", self.userProvidedHue);
+    });
+
+    //INVERT
+    let filterButton_invert = document.getElementById("filter_button_invert");
+    let invertInput = document.getElementById("invertnum");
+    this.userProvidedInvert = 0;
+
+    filterButton_invert.addEventListener("click", function () {
     self.userProvidedInvert = invertInput.value;
     console.log("invert:", self.userProvidedInvert);
 });
@@ -37,8 +58,10 @@ filterButton_invert.addEventListener("click", function () {
   
   display() {
     this.context.save();
-    this.context.filter = `blur(${this.userProvidedBlur}px)`;this.context.filter += ` invert(${this.userProvidedInvert})`;
-    //this.context.filter += `hue-rotate(${userProvidedDeg}px)`
+    this.context.filter = `blur(${this.userProvidedBlur}px)`;
+    this.context.filter += ` invert(${this.userProvidedInvert})`;
+    this.context.filter += ` sepia(${this.userProvidedSepia})`;
+    this.context.filter += ` hue-rotate(${this.userProvidedHue}deg)`;
 
     this.context.drawImage(this.videoElement, this.x, this.y, this.w, this.h);
     this.context.fillStyle = this.shapeCol;
